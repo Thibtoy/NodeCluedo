1361d5ee1df01d6f17193f5db15c75a05c34176c
Thibtoy <cabanes.thibault@gmail.com>
2019-10-19 14:43:29 +0200

    bug fix

--- cluedo/game/class/player.js
+++ cluedo/game/class/player.js
@@ -15,33 +15,6 @@ class Player {
 			},
 		}
 	}
-
-	// throwDice = (nb) => {
-	// 	let results = new Array();
-
-	// 	for (let i = 0, l = nb; i < l; i++){ 
-	// 		results.push(Math.ceil(Math.random()*6));			
-	// 	}
-
-	// 	for (let i = 0, l = nb; i < l; i++){
-	// 		if (results[i] === 1 || results[i] === 6) {
-	// 			for (let j = 0; j < l; j++){
-	// 				if (j === i) continue;
-	// 				if (results[j] === results[i]) {
-	// 					this.state.double = true;
-	// 				}
-	// 			}
-	// 		}
-	// 		this.state.mouve += results[i];
-	// 	}
-	// 	return this.state.dices = results;
-	// }
-
-// 	makeAccusation = (accusation, game) => {
-// 		this.state.accusation = true;
-// 		if (accusation === game.truth) console.log('gameEnd');
-// 		else console.log('nextTurn');
-// 	}
 }
 
 module.exports = {
