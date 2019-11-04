066fac9fa6cc5a168f2a3906c48e0511e41e418b
Thibtoy <cabanes.thibault@gmail.com>
2019-11-04 01:19:51 +0100

    blalba

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var sqlayout = require('../sqlayout');
+sqlayout = sqlayout({
+	host: process.env.DATABASE_URL || 'localhost',
+	user: process.env.DATABASE_USER || 'root',
+	password: process.env.DATABASE_PASS || 'root',
+	database: process.env.DATABASE_NAME || 'sqlayout',
+});
+
+userModel = {
+	table: 'users',
+	id: {type: 'INT', autoInc: true, primaryKey: true},
+	name: {type: 'VARCHAR(55)', restriction: /^[a-zA-Z- ]+$/},
+	test: {type: 'VARCHAR(50)', nullable: true},
+	password: {type: 'PASSWORD'},
+	date: {type: 'TIMESTAMP'},
+	email: {type: 'EMAIL'}
+};
+
+testModel = {
+	table: 'test',
+	id: {type: 'INT', autoInc: true, primaryKey: true},
+	categorie: {type: 'VARCHAR(55)'},
+	user_id: {type: 'INT', foreignKey: {refTable: 'users', refField: 'id'}}
+};
+
+sqlayout.initModel(userModel);
+sqlayout.initModel(testModel);
+
+
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -33,5 +63,28 @@ gameSchema().catch(err => console.log(err))
 	  			}
 	  		}, 600000);
 
-			app.listen(PORT, () => console.log('listening on: '+PORT));
+			app.listen(PORT, () => {
+				console.log('listening on: '+PORT);
+				// newUser = sqlayout.models.users.requireModel();
+				// newUser.name.content = 'test';
+				// newUser.password.content = 'LeMotDeP4ssePasSiCompl?qu';
+				// newUser.date.content = 20181113220654;
+				// newUser.email.content = 'cabanes.thibault@gmail.com';
+				newTest = sqlayout.models.test.requireModel();
+				newTest.categorie.content = 'another test';
+				newTest.user_id.content = 1;
+				sqlayout.models.test.recordSafe(newTest, function(err, data) {
+					if (err) return console.log(err);
+					sqlayout.models.users.findAll()
+						.then(data => {
+							console.log(data, 'ici');
+							data[0].getTest()
+								.then (data => console.log(data))
+								.catch(err => {throw err});
+						})
+						.catch(err => {throw err});
+				});
+				
+				
+			});
 		});
